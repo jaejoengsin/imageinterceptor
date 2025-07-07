ad304a416787ede1eb8b9246f778e53f2d1c8a81
최재정 <zhzhqkq0226@gmail.com>
2025-07-07 21:23:17 +0900

    Add web accessible resource for image extract module

--- src/js/content_scripts.js
+++ src/js/content_scripts.js
@@ -1,1 +1,11 @@
-console.log("확장프로그램이다.")
+console.log("확장프로그램이다.");
+
+// Dynamically import the module that installs the image interceptor.
+const moduleUrl = chrome.runtime.getURL('/src/js/utils/image_extract.js');
+import(moduleUrl)
+  .then(({ installImageInterceptor }) => {
+    if (typeof installImageInterceptor === 'function') {
+      installImageInterceptor();
+    }
+  })
+  .catch(err => console.error('Failed to load image_extract module', err));
