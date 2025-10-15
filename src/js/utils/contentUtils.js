498f059b9f1210234b69015cfb5088ce8a8b68bd
jaejoengsin <zhzhqkq0226@gmail.com>
2025-10-16 00:30:15 +0900

    fix 대체 이미지 생성 코드 수정  취소

--- src/js/utils/contentUtils.js
+++ src/js/utils/contentUtils.js
@@ -5,14 +5,27 @@ const harmfulImgMark = chrome.runtime.getURL('icons/main_icon.png');
 export function changeImg(img, flag) {
     if (flag) {
         img.src = harmfulImgMark;
+        //기존 코드
         img.style.backgroundColor = 'white';
         img.style.objectFit = 'contain';
         img.style.objectPosition = 'center';
+        // img.style.position = "absolute";          // 절대 위치
+        // img.style.top = "0";                      // 상단
+        // img.style.right = "0";                    // 우측
+        // img.style.width = "50px";                 // 최대 폭
+        // img.style.height = "50px";                // 최대 높이
+        
+        img.style.background = "white"; 
     }
     else {
         img.style.backgroundColor = '';
         img.style.objectFit = '';
         img.style.objectPosition = '';
+        //추가
+        // img.style.position = ""; 
+        // img.style.width = "";                 // 최대 폭
+        // img.style.height = "";  
+        //
         img.src = img.dataset.originalSrc;
 
     }
